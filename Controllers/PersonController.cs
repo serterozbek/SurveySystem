231e86e4c5fc750d4db387fe240e169d84774c82
serterozbek <serter.ozbek@hotmail.com>
2022-04-26 23:51:55 +0300

    Veritabanına Ekleme işlemi ve login sayfaları oluşturuldu. video5

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using Survey_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Survey_System.Controllers
+{
+    public class PersonController : Controller
+    {
+        survey_systemEntities db = new survey_systemEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create(Person person)
+        {
+            if (person.NameSurname != null)
+            {
+                person.CreateDate = DateTime.Now;
+                person.CreateBy = "System";
+                db.Person.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+            return View();
+            }
+
+        }
+
+
+    }
+}
